319882c83e857672df2c1a9cc18387174a006488
Subedara <abd2maqsood@gmail.com>
2024-11-22 15:20:50 +0500

    commit main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+print("""Hello to Ultra OS 1.0
+    In this OS, there are some 
+    Ultra specific features like
+    opening mp3, mp4, apk, exe
+    and many more files.
 
-username = input("Enter your usename: ")
+    Many features are still in
+    development, so you might
+    have to wait for Ultra OS 1.5
+    or Ultra OS 2.0.
+
+    --Subedara
+
+""")
+beginnerprompt = input("Make a new account? ")
+if beginnerprompt == "yes":
+    username = input("Enter your usename: ")
+    password = input("Enter a secure password: ")
+else:
+    print("Okay, EXITING...")
